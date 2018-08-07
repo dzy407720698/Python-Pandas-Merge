5dd7fa4e45fbd562cb4752d6c3d0cd493eaa2435
Annette Chiu <lian600058@gmail.com>
2018-08-07 10:46:48 -0400

    Python Pandas Remove and Rename

---
+++ Python+Pandas+Remove+Columns+and+rename.py
@@ -0,0 +1,197 @@
+
+# coding: utf-8
+
+# In[115]:
+
+import pandas as pd
+import numpy as np
+df = pd.read_csv('/Users/annettechiu/Desktop/2018_Every_Library_Intership/pupld11b.csv')
+df11= df[['STABR','PHONE','LIBNAME','LOCGVT']]
+df11= df11.rename(columns={'LOCGVT':'2011 LOCGVT'})
+df11.to_csv('2011.csv')
+
+
+# In[85]:
+
+import pandas as pd
+import numpy as np
+df = pd.read_csv('/Users/annettechiu/Desktop/2018_Every_Library_Intership/pupld10a.csv')
+df
+df10= df[['STABR','PHONE','LIBNAME','LOCGVT']]
+df10.to_csv('2010.csv')
+
+
+# In[86]:
+
+import pandas as pd
+import numpy as np
+df = pd.read_csv('/Users/annettechiu/Desktop/2018_Every_Library_Intership/pupld09a.csv')
+df
+df09= df[['STABR','PHONE','LIBNAME','LOCGVT']]
+df09.to_csv('2009.csv')
+
+
+# In[87]:
+
+import pandas as pd
+import numpy as np
+df = pd.read_csv('/Users/annettechiu/Desktop/2018_Every_Library_Intership/pupld08.csv')
+df
+df08= df[['STABR','PHONE','LIBNAME','LOCGVT']]
+df08.to_csv('2008.csv')
+
+
+# In[92]:
+
+import pandas as pd
+import numpy as np
+df = pd.read_csv('/Users/annettechiu/Desktop/2018_Every_Library_Intership/pupld06a.csv')
+df06= df[['STABR','PHONE','LIBNAME','LOCGVT']]
+df06.to_csv('2006.csv')
+
+
+# In[94]:
+
+import pandas as pd
+import numpy as np
+df = pd.read_csv('/Users/annettechiu/Desktop/2018_Every_Library_Intership/pupld05a.csv')
+df
+df05= df[['STABR','PHONE','LIBNAME','LOCGVT']]
+df05.to_csv('2005.csv')
+
+
+# In[ ]:
+
+import pandas as pd
+import numpy as np
+df = pd.read_csv('/Users/annettechiu/Desktop/2018_Every_Library_Intership/pupld04a.csv')
+df
+df04= df[['STABR','PHONE','LIBNAME','LOCGVT']]
+df04.to_csv('2004.csv')
+
+
+# In[97]:
+
+import pandas as pd
+import numpy as np
+df = pd.read_csv('/Users/annettechiu/Desktop/2018_Every_Library_Intership/pupld03a.csv')
+df
+df03= df[['STABR','PHONE','LIBNAME','LOCGVT']]
+df03.to_csv('2003.csv')
+
+
+# In[98]:
+
+import pandas as pd
+import numpy as np
+df = pd.read_csv('/Users/annettechiu/Desktop/2018_Every_Library_Intership/pupld02a.csv')
+df
+df02= df[['STABR','PHONE','LIBNAME','LOCGVT']]
+df02.to_csv('2002.csv')
+
+
+# In[102]:
+
+import pandas as pd
+import numpy as np
+df = pd.read_csv('/Users/annettechiu/Desktop/2018_Every_Library_Intership/pupld01a.csv')
+df01= df[['STABR','PHONE','LIBNAME','LOCGVT']]
+df01.to_csv('2001.csv')
+
+
+# In[105]:
+
+import pandas as pd
+import numpy as np
+df = pd.read_csv('/Users/annettechiu/Desktop/2018_Every_Library_Intership/pupldf00a.csv')
+df00= df[['STABR','PHONE','LIBNAME','LOCGVT']]
+df00.to_csv('2000.csv')
+
+
+# In[107]:
+
+import pandas as pd
+import numpy as np
+df = pd.read_csv('/Users/annettechiu/Desktop/2018_Every_Library_Intership/pupldf99.csv')
+
+df99= df[['STABR','PHONE','LIBNAME','LOCGVT']]
+df99.to_csv('1999.csv') 
+
+
+# In[108]:
+
+import pandas as pd
+import numpy as np
+df = pd.read_csv('/Users/annettechiu/Desktop/2018_Every_Library_Intership/pupldf98.csv')
+df98= df[['STABR','PHONE','LIBNAME','LOCGVT']]
+df98.to_csv('1998.csv')
+
+
+# In[ ]:
+
+import pandas as pd
+import numpy as np
+df = pd.read_csv('/Users/annettechiu/Desktop/2018_Every_Library_Intership/pupldf97.csv')
+df97= df[['STABR','PHONE','LIBNAME','LOCGVT']]
+df97.to_csv('1997.csv')
+
+
+# In[ ]:
+
+import pandas as pd
+import numpy as np
+df = pd.read_csv('/Users/annettechiu/Desktop/2018_Every_Library_Intership/pupldf96.csv')
+df
+df96= df[['STABR','PHONE','LIBNAME','LOCGVT']]
+df96.to_csv('1996.csv')
+
+
+# In[ ]:
+
+import pandas as pd
+import numpy as np
+df = pd.read_csv('/Users/annettechiu/Desktop/2018_Every_Library_Intership/pupldf95.csv')
+df
+df95= df[['STABR','PHONE','LIBNAME','LOCGVT']]
+df95.to_csv('1995.csv')
+
+
+# In[ ]:
+
+import pandas as pd
+import numpy as np
+df = pd.read_csv('/Users/annettechiu/Desktop/2018_Every_Library_Intership/pupldf94.csv')
+df
+df94= df[['STABR','PHONE','LIBNAME','LOCGVT']]
+df94.to_csv('1994.csv')
+
+
+# In[ ]:
+
+import pandas as pd
+import numpy as np
+df = pd.read_csv('/Users/annettechiu/Desktop/2018_Every_Library_Intership/pupldf93.csv')
+df
+df93= df[['STABR','PHONE','LIBNAME','LOCGVT']]
+df93.to_csv('1993.csv')
+
+
+# In[ ]:
+
+import pandas as pd
+import numpy as np
+df = pd.read_csv('/Users/annettechiu/Desktop/2018_Every_Library_Intership/pupldf92.csv')
+df
+df92= df[['STABR','PHONE','LIBNAME','LOCGVT']]
+df92.to_csv('1992.csv')
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
